6f62607b73ab9beffc4600da38b707c3300d7ca2
Kelvin Kemper <sorlorg@unm.edu>
2023-02-16 21:07:00 -0700

    finished exam review

--- Exam1Review.rkt
+++ Exam1Review.rkt
@@ -1,9 +1,15 @@
 #lang racket
 
+;; every value is consider #t such as 'cat 'dog 5
+;; only #f (nothing else) is false in scheme 
+
 ;; TAIL POSITIONS
 
 ;; understand pair? vs list?
 ;; eq? equal? eqv? (=)
+;; (=) for NUMBERS only!
+;; eq? -> strict, compares pointers in memory location **pointers
+;; equal? can be used for lists **lists
 
 
 ;; defining bindings as it evaluates the expression, then bindings are gone
@@ -22,7 +28,9 @@
       ((names (map car (cadr expr)))
       (vals  (map cadr (cadr expr)))
       (body  (caddr expr)))
-    (cons (cons 'lambda (cons names (cons body '()) vals))))))
+    (cons (cons 'lambda (cons names (cons body '()))) vals))))
+
+
 
 
 (define deep-flatten
@@ -82,3 +90,85 @@
   (lambda (x)
     (letrec ((helper (lambda (y acc) (if (> y 0) (helper (- y 1) (+ y acc)) acc))))
       (helper x 0))));; starting with the accumulator at zero
+
+
+
+(define foo (cons 1 2))
+(define bar (cons 1 2))
+(define bert 'foo)
+(define ernie bar)
+
+
+
+
+; list - can be an empty list or a pair whose second element is a list, must have '() at end
+; pair - combination of two values
+; append - must combine a list with atom/list
+;; equal? - 
+
+(list? (cons 1 '()))
+;; #t - lists checks if it's a list that ends with '()
+
+(let* ((x (cons 1 2)) (y x)) 
+      (eq? x y))
+;; #t - x = (cons 1 2), y = x = (cons 1 2) y points to x that poitns to (cons 1 2)
+
+;(append 1 (list 2 3))
+;; #f 1st argument must be a list, 2nd can be either list or atom
+
+(and 'cat #t #f) 
+;; #f contains a false
+
+((lambda (x y) y) 2 3)
+;; 3
+
+(if 1 2 3)
+;; if (first thing = true? true is anything excep #f) then 2 otherwise 3
+
+;(1 2)
+;; no procedure or special case
+
+(eq? (cons 'kirk 'spock) (cons 'kirk 'spock))
+;; #f - different pointer locations
+
+(equal? 1 1)
+;; #t
+
+;(append (1 2) (3 4))
+;; error since two arguments are not list must have (quote) or '() around arguments
+
+(cons 1 2)
+;; '(1 . 2)
+;(cadr 1 2)
+;; incorrect argument mismatch
+
+(or #t #f 'dog)
+;; #t
+
+(cond (#f 0) (#t 1) (else 2))
+;; 1 since always true
+
+(cadr (list 1 2 3))
+;; 2
+
+(car '(((1))))
+; '((1))
+
+(cdr '(((1))))
+; '()
+
+(list? (cons 1 2))
+;#f
+
+(list 1 2 3)
+;'(1 2 3)
+
+(append)
+; '() - identity
+
+
+(+)
+; 0
+
+(*)
+; 1
